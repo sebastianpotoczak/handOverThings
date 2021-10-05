7ebff3b3b61c147a53582219947b68247e01a1ad
sebastianpotoczak <sebastianpotoczak3@wp.pl>
2021-10-05 15:47:19 +0200

    do contact

--- src/component/aboutUs.js
+++ src/component/aboutUs.js
@@ -1,7 +1,30 @@
+import tribal from '../assets/Decoration.svg'
+import signature from '../assets/Signature.svg'
+import peoplePhoto from '../assets/People.jpg'
+
 const AboutUs = () => {
     return(
         <>
-            <h1>O nas</h1>
+            <div className="aboutMe">
+                <div className="aboutContain">
+                <div className="leftAbout">
+                        <h1>O nas</h1>
+                        <img src={tribal} />
+                        <p>Nori winogrono srebro burak brokuły kombu buraki fava fasola ziemniaki quandong seler. Orzechy Bunya Czarnooki Groch Prairie Rzepa Por Soczewica Rzepa Zieleń Pasternak.</p>
+                        <div className="signature">
+
+                        <img src={signature} />
+                        </div>
+                       
+                    
+                    </div>
+
+                </div>
+                <div className="rightAbout">
+                    <img src={peoplePhoto} />
+                </div>
+            </div>
+
         </>
     )
 }
--- src/component/contact.js
+++ src/component/contact.js
@@ -1,4 +1,4 @@
-
+import tribal from '../assets/Decoration.svg'
 
 
 
@@ -6,7 +6,30 @@
 const Contact = () => {
     return(
         <>
-            <h1>Skontaktuj sie z nami!</h1>
+            <div className="contact">
+                <div className="contact_contain">
+                    <div className="contact_text">
+                        <h1>Skontaktuj się z nami</h1>
+                        <img src={tribal}/>
+                        <div className="contact_name">
+                            <div>
+                            <p>Wpisz swoje imię</p>
+                            <input />
+
+                            </div>
+                            <div>
+                            <p>Wpisz swój email</p>
+                            <input />
+
+                            </div>
+                         
+                         
+                        </div>
+                        Wpisz swoją wiadomość
+                        <textarea />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
