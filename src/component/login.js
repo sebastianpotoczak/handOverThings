2acbce97e0345b59dd7c5f21f3ae604891b1b561
sebastianpotoczak <sebastianpotoczak3@wp.pl>
2021-10-08 14:43:32 +0200

    Validation login and register

--- src/component/login.js
+++ src/component/login.js
@@ -1,6 +1,48 @@
 import Nav from './nav';
 import tribal from '../assets/Decoration.svg'
-const loginView = () => {
+import { useState } from 'react'
+import validator from 'validator'
+
+const LoginView = () => {
+
+  const[loginEmail, setLoginEmail] = useState("");
+  const[loginPassword, setLoginPassword] = useState("");
+  const[displayLogin, setDisplayLogin] = useState("none");
+  const[validateEmail, setValidateEmail] = useState(false)
+
+
+  const loginStyle = {
+    display: displayLogin
+  }
+
+
+  const handleEmail = (e) => {
+    const newEmail = e.target.value;
+    setLoginEmail(newEmail)
+
+    if(validator.isEmail(newEmail)){
+      setValidateEmail(true)
+    }
+
+ 
+  }
+
+  const handlePassword = (e) => {
+      const newPassword = e.target.value 
+      setLoginPassword(newPassword)
+  }
+
+
+
+const handleClick = (e) => {
+  if( validateEmail === false || loginPassword === "" || validateEmail === "" || loginPassword.length <= 6){
+    setDisplayLogin("flex")
+    e.preventDefault()
+  }
+}
+
+
+
     return(
       <>
       <div className="login">  
@@ -16,21 +58,21 @@ const loginView = () => {
               <div className="login_input">
                   <div className="login_element">
                       <p>Email</p>
-                     <input />
+                     <input onChange={handleEmail} value={loginEmail}/>
+                     <h3 className="login_error" style={loginStyle} >Podany email jest nieprawidłowy!</h3>
                   </div>
            
-                  <div className="login_element">
+                  <div className="login_element" >
                       <p>Hasło</p>
-                    <input />
+                    <input type="password" value={loginPassword} onChange={handlePassword}/>
+                    <h3 className="login_error" style={loginStyle}  >Podane hasło jest błędne</h3>
                   </div>
                 <div>  
                 </div>
-              
-
               </div>  
               <div className="login_submit">
                 <a href="http://localhost:3000/#/rejestracja">Załóż konto</a>   
-                    <a href="#">Zaloguj się</a>
+                    <a href="#" onClick={handleClick}>Zaloguj się</a>
 
               </div>
                    
@@ -48,4 +90,4 @@ const loginView = () => {
 }
 
 
-export default loginView
+export default LoginView
--- src/component/register.js
+++ src/component/register.js
@@ -1,7 +1,54 @@
 import Nav from "./nav"
 import tribal from '../assets/Decoration.svg'
+import { useState } from 'react'
+import validator from 'validator'
 
 const Register = () => {
+
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const[passwordRepeat, setPasswordRepeat] = useState("");
+  const[display, setDisplay] = useState("none")
+  const[validateEmail, setValidateEmail] = useState(false)
+
+const registerError = {
+  display: display
+}
+
+
+
+const handleEmail = (e) => {
+  const newEmail = e.target.value;
+  setEmail(newEmail)
+
+  if(validator.isEmail(newEmail)){
+    setValidateEmail(true)
+  }
+
+
+}
+
+  const handlePassword = (e) => {
+    const newPassword = e.target.value
+    setPassword(newPassword)
+
+  }
+  const handlePasswordRepeat = (e) => {
+    const newPasswordRepeat = e.target.value
+    setPasswordRepeat(newPasswordRepeat)
+
+  }
+
+
+  const handleClick = (e) => {
+    if( validateEmail === false || password === "" || passwordRepeat === ""  || password != passwordRepeat){
+      setDisplay("flex")
+      e.preventDefault()
+    }
+   
+  }
+
+
     return(
         <>
         <div className="login">  
@@ -17,18 +64,21 @@ const Register = () => {
                 <div className="register_input">
                     <div className="login_element">
                         <p>Email</p>
-                       <input />
+                       <input value={email} onChange={handleEmail} type="text"/>
+                       <h3 className="register_error" style={registerError}>Podaj poprawny email</h3>
                     </div>
              
                     <div className="login_element">
                         <p>Hasło</p>
-                      <input />
+                      <input type="password" value={password} onChange={handlePassword}/>
+                      <h3 className="register_error" style={registerError}>Hasło powinno mieć 6 znaków</h3>
                     </div>
 
 
                     <div className="login_element">
                         <p>Powtórz hasło</p>
-                      <input />
+                      <input type="password" value={passwordRepeat} onChange={handlePasswordRepeat}/>
+                      <h3 className="register_error" style={registerError}>Hasła muszą byc takie same</h3>
                     </div>
                   <div>  
                   </div>
@@ -37,7 +87,7 @@ const Register = () => {
                 </div>  
                 <div className="login_submit">
                   <a href="http://localhost:3000/#/login">Zaloguj się</a>   
-                      <a href="#">Załóż konto</a>
+                      <a href="http://localhost:3000/#/" onClick={handleClick}>Załóż konto</a>
   
                 </div>
                      
