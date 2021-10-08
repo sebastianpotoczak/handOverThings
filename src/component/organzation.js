6c621cbe5ceac24732cea1159faed5074f7be611
sebastianpotoczak <sebastianpotoczak3@wp.pl>
2021-10-08 21:47:28 +0200

    Organization

--- src/component/organzation.js
+++ src/component/organzation.js
@@ -1,18 +1,44 @@
 import tribal from '../assets/Decoration.svg'
 import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { useEffect, useState } from 'react';
+
+
+const buttons = [
+    "fundacion",
+    "organisations",
+    "other"
+]
+
 const Concern = () => {
+    const [fundations, setFundations] = useState([]);
+    const [current, setCurrent] = useState("fundacion");
+
+    useEffect(() => {
+        fetch(`http://localhost:3005/${current}`)
+            .then(res => res.json())
+            .then(res => setFundations(res))
+    }, [current])
+
+
+    const handleCurrent = fund => () => {
+        setCurrent(fund)
+    }
+
+    console.log(fundations)
+
     return(
         <>
         <Element name="company">
             <div className="concern">
+              =
                 <div className="concern_contain"> 
                     <div className="concern_tittle">
                         <h1>Komu pomagamy?</h1>
                         <img src={tribal}/>
-                        <div>
-                        <a href="#">Fundacjom</a>
-                        <a href="#">Organizacjom pozarządowym</a>
-                        <a href="#">Lokalnym zbiórkom</a>
+                        <div className="concern_position">
+                        {buttons.map(button => (
+                    <a href="#" onClick={handleCurrent(button)}>{button}</a>
+                ))}
 
                         </div>
                        
@@ -20,36 +46,13 @@ const Concern = () => {
 
                     </div>
                     <div className="concern_content">
-                       <div>
-                            <div className="concern_element">
-
-                            <h3>Fundacja "Dbam o Zdrowie"</h3>
-                        <p>Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.</p>
-
-                        </div>
-                        <h4>ubrania,jedzenie, sprzęd AGD, meble ,zabawki</h4>
-                        </div>
-                        <div> 
-                            <div className="concern_element">
-                            <h3>Fundacja “Dla dzieci”</h3>
-                        <p>Cel i misja: Pomoc dzieciom z ubogich rodzin.</p>
-
-                            </div>
-                            <h4>ubrania, meble , zabawki</h4>
-                        
-
-                        </div>
-                        <div> 
-                            <div className="concern_element">
-                            <h3>Fundacja “Bez domu”</h3>
-                            <p>Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.</p>
-
-                            </div>
-                            <h4>ubrania, jedzenie, ciepłe koce</h4>
-                                <a href="#">1</a>
-                                <a href="#">2</a>
-                                <a href="#">3</a>
-                        </div>
+                        {fundations.map(fund => (
+                            <>
+                            <h2>{fund.title}</h2>
+                            <p>{fund.text}</p>
+                            <i>{fund.items}</i>
+                            </>
+                        ))}
                       
 
 
